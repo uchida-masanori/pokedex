3bf18fa6f641700eaa8f737994ffddad6a441c6c
uchida/masanori <76242252+uchida-masanori@users.noreply.github.com>
2021-07-12 02:44:07 +0900

    addFirebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBxoakL9xc1-4y_0NCmG4ToAXz6m6OOsMM",
+    authDomain: "pokedex-2cdf6.firebaseapp.com",
+    projectId: "pokedex-2cdf6",
+    storageBucket: "pokedex-2cdf6.appspot.com",
+    messagingSenderId: "595156012759",
+    appId: "1:595156012759:web:454796478a546fc9472a92",
+    measurementId: "G-JMDH7K2MVR"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+    firebase: {
+    apiKey: "AIzaSyBxoakL9xc1-4y_0NCmG4ToAXz6m6OOsMM",
+    authDomain: "pokedex-2cdf6.firebaseapp.com",
+    projectId: "pokedex-2cdf6",
+    storageBucket: "pokedex-2cdf6.appspot.com",
+    messagingSenderId: "595156012759",
+    appId: "1:595156012759:web:454796478a546fc9472a92",
+    measurementId: "G-JMDH7K2MVR"
+  }
 };
 
 /*
