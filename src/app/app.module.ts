692f44fddc28d01c60fbd77330c8a7a2e10f19ac
uchida-masanori <76242252+uchida-masanori@users.noreply.github.com>
2021-07-12 01:14:15 +0900

    addRadarCharts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ChartsModule } from 'ng2-charts';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { PokemonDetailComponent } from './pokemon-detail/pokemon-detail.component';
+import { PokemonListComponent } from './pokemon-list/pokemon-list.component';
 import { TypePipe } from './pypes/type.pipe';
 
 
@@ -22,7 +23,8 @@ import { TypePipe } from './pypes/type.pipe';
     AppRoutingModule,
     BrowserAnimationsModule,
     MatToolbarModule,
-    MatCardModule
+    MatCardModule,
+    ChartsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pokemon-detail/pokemon-detail.component.ts
+++ src/app/pokemon-detail/pokemon-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { ChartDataSets, ChartType, RadialChartOptions } from 'chart.js';
+import { Label } from 'ng2-charts';
 import { POKEMONS } from '../models/pokemons';
 
 @Component({
@@ -13,6 +15,17 @@ export class PokemonDetailComponent implements OnInit {
 
   pokemon!: { id: number; name: { english: string; japanese: string; chinese: string; french: string; }; type: string[]; base: { HP: number; Attack: number; Defense: number; "Sp. Attack": number; "Sp. Defense": number; Speed: number; }; };
 
+  public radarChartOptions: RadialChartOptions = {
+    responsive: true,
+    legend: {
+      display: false
+    }
+  };
+  public radarChartLabels: Label[] = ['HP', 'こうげき', 'ぼうぎょ', 'とくこう', 'とくぼう', 'すばやさ'];
+
+  public radarChartData: ChartDataSets[] = [];
+  public radarChartType: ChartType = 'radar';
+
   constructor(
     private route: ActivatedRoute,
   ) { }
@@ -20,7 +33,17 @@ export class PokemonDetailComponent implements OnInit {
   ngOnInit(): void {
     this.route.paramMap.subscribe(map => {
       const id = Number(map.get('id'));
-      this.pokemon = this.pokemons[id - 1]
+      this.pokemon = this.pokemons[id - 1];
+      this.radarChartData.push({
+        data: [
+          this.pokemon.base.HP,
+          this.pokemon.base.Attack,
+          this.pokemon.base.Defense,
+          this.pokemon.base['Sp. Attack'],
+          this.pokemon.base['Sp. Defense'],
+          this.pokemon.base.Speed,
+        ]
+      })
     })
   }
 }
