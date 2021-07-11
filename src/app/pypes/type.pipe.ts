79ddefce26fe66c62c54d22041cf49ed666e8020
uchida-masanori <76242252+uchida-masanori@users.noreply.github.com>
2021-07-12 01:30:47 +0900

    addType

--- src/app/pypes/type.pipe.ts
+++ src/app/pypes/type.pipe.ts
@@ -6,9 +6,24 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TypePipe implements PipeTransform {
 
   japanese: { [index: string]: string; } ={
-    Grass: '草',
-    Poison: '毒'
-
+    Grass: 'くさ',
+    Poison: 'どく',
+    Fire: 'ほのお',
+    Flying: 'ひこう',
+    Water: 'みず',
+    Bug: 'むし',
+    Normal: 'ノーマル',
+    Ground: 'じめん',
+    Fairy: 'フェアリー',
+    Fighting: 'かくとう',
+    Psychic: 'エスパー',
+    Ghost: 'ゴースト',
+    Rock: 'いわ',
+    Electric: 'でんき',
+    Dark: 'あく',
+    Dragon: 'ドラゴン',
+    Ice: 'こおり',
+    Steel: 'はがね'
   };
 
   transform(types: string[], ...args: any[]): any {
